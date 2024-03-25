20e554a72247d0ffbf8ebfc31ea6d02a5c78ba66
ModyQyW <wurui.dev@gmail.com>
2024-03-25 09:14:34 +0800

    chore: add nitro link

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # openrunner-clash-freenode
 
-This is a simple service built with nitro that requests [OpenRunner/clash-freenode](https://github.com/OpenRunner/clash-freenode) for the latest clash / v2ray nodes, **for learning purposes only**.
+This is a simple service built with [nitro](https://nitro.unjs.io/) that requests [OpenRunner/clash-freenode](https://github.com/OpenRunner/clash-freenode) for the latest clash / v2ray nodes, **for learning purposes only**.
 
 Request /clash for latest clash nodes and /v2ray for latest v2ray nodes.
 
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,3 +1,3 @@
 export default defineEventHandler(() => {
-  return "This is a simple service built with nitro that requests [OpenRunner/clash-freenode](https://github.com/OpenRunner/clash-freenode) for the latest clash / v2ray nodes, for learning purposes only. Request /clash for latest clash nodes and /v2ray for latest v2ray nodes.";
+  return "This is a simple service built with [nitro](https://nitro.unjs.io/) that requests [OpenRunner/clash-freenode](https://github.com/OpenRunner/clash-freenode) for the latest clash / v2ray nodes, for learning purposes only. Request /clash for latest clash nodes and /v2ray for latest v2ray nodes.";
 });
